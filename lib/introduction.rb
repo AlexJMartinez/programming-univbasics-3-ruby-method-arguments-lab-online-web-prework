0bf4078e4eabc511355498681bc1fb800ff4a710
Alex Martinez <alexandr3martin3z@gmail.com>
2019-09-22 02:54:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,9 +6,10 @@ def introduction_with_language(josh, react)
   puts "Hi, my name is #{josh} and I am learning to program in #{react}."
 end
 
-def introduction_with_language_optional(edwin, language="Ruby")
-  puts "Hi, my name is #{edwin} and I am learning to program in #{ruby}."
+def introduction_with_language_optional(Edwin, language="Ruby")
+  puts "Hi, my name is #{Edwin} and I am learning to program in #{Ruby}.
 end
+
 
 
 
